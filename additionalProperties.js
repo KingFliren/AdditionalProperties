adc1cf7d839d5f2848214dfb70363f56832aef91
KingFliren <95429696+KingFliren@users.noreply.github.com>
2024-03-27 13:52:17 +0100

    Add files via upload

---
+++ additionalProperties.js
@@ -0,0 +1,8 @@
+Hooks.on('ready', () => {
+CONFIG.DND5E.weaponProperties['impact'] = 'Impact';
+CONFIG.DND5E.weaponProperties['impale'] = 'Impale';
+CONFIG.DND5E.weaponProperties['cylinderloaded'] = 'Cylinder Loaded';
+CONFIG.DND5E.weaponProperties['scatter'] = 'Scatter';
+CONFIG.DND5E.weaponProperties['doublebarrel'] = 'Double Barrel';
+CONFIG.DND5E.weaponProperties['precise'] = 'Precise';
+});
